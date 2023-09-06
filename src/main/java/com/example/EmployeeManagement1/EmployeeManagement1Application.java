a135c7e42c892c6fe5385dc546c65732a2384169
deepshikhavishwakarma <deepshikha2000deep@gmail.com>
2023-09-06 22:46:26 +0530

    Springboot project

---
+++ src/main/java/com/example/EmployeeManagement1/EmployeeManagement1Application.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeManagement1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagement1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EmployeeManagement1/EmployeeManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeManagement1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
